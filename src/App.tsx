e21ae0273dd0b9b94e163045e02c5d4d360f847e
ederby <ederby4@gmail.com>
2024-12-18 12:19:28 +0100

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import PageNotFound from "./pages/PageNotFound";
+import Services from "./pages/Services";
+import Calendar from "./pages/Calendar";
+import Settings from "./pages/Settings";
+import AppLayout from "./ui/AppLayout";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Navigate replace to="dashboard" />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="*" element={<PageNotFound />} />
+        <Route element={<AppLayout />}>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="services" element={<Services />} />
+          <Route path="calendar" element={<Calendar />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Calendar.tsx
@@ -0,0 +1,3 @@
+export default function Calendar() {
+  return <div className="text-3xl">Calendar</div>;
+}
